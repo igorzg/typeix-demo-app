ac75f52ce8e8671a16a265739ad46d2e097a3ecf
igorzg <igor.zg1987@gmail.com>
2017-03-10 11:44:40 +0100

    Update of tests

--- tests/intergration/home-controller.spec.ts
+++ tests/intergration/home-controller.spec.ts
@@ -1,64 +1,35 @@
-import {fakeControllerActionCall, Injector, Router, Logger} from "typeix";
+import {fakeHttpServer, FakeServerApi} from "typeix";
 import {assert, use} from "chai";
 import * as sinonChai from "sinon-chai";
 import {spy, assert as assertSpy} from "sinon";
-import {HomeController} from "../../controllers/home";
-import {Assets} from "../../components/assets";
+import {Application} from "../../application";
 
 // use chai spies
 use(sinonChai);
 
 describe("Home controller", () => {
 
-    it("Should test id action", (done) => {
-        let id = 1;
-        let name = "Igor";
-        let injector = Injector.createAndResolve(Assets, [Logger, Router]);
-        fakeControllerActionCall(
-            injector,
-            HomeController,
-            "id",
-            {
-                name,
-                id
-            }
-        ).then(data => {
-            assert.equal(data, `After cache controller filter <- Action id: ${id} name: ${name} <- Before each core <- Before cache controller filter <-null`);
+    var server: FakeServerApi;
+
+    before(() => {
+        server = fakeHttpServer(Application);
+    });
+
+    function template(title, name, id) {
+        return `<!DOCTYPE html>\n<html lang="en">\n<head>\n    <meta charset="UTF-8">\n    <title>${title}</title>\n</head>\n<body>\n<h4>Headline: ${name}</h4>\n<p>\n    Action id: ${id} name: ${name} <- Before each core &lt;- Before cache controller filter &lt;-null;\n</p>\n</body>\n</html>\n`;
+    };
+
+    it("Should test index action", (done) => {
+        server.GET("/").then(data => {
+            assert.equal(data.getBody(), template("Home page example", "this is home page", "NO_ID"));
             done();
         }).catch(done);
     });
 
 
-    it("Should index action", (done) => {
-        let result = "After cache controller filter <- Action index home: <- Before each core <- Before cache controller filter <-null";
-        let injector = Injector.createAndResolve(Assets, [Logger, Router]);
-        fakeControllerActionCall(
-            injector,
-            HomeController,
-            "index"
-        ).then(data => {
-            assert.equal(data, result);
-            done();
-        }).catch(done);
-    });
-
-
-
-    it("Should redirect action", (done) => {
-        let mock = {
-            createUrl: () => {}
-        };
-        let aSpy = spy(mock, "createUrl");
-        let injector = Injector.createAndResolve(Assets, [
-            Logger,
-            {provide: Router, useValue: mock}
-        ]);
-        fakeControllerActionCall(
-            injector,
-            HomeController,
-            "redirect"
-        ).then(data => {
-            assertSpy.calledWith(aSpy, "home/index", {});
+    it("Should test home id action", (done) => {
+        server.GET("/100/whatevericansee").then(data => {
+            assert.equal(data.getBody(), template("Template engine with typeix", "whatevericansee", "100"));
             done();
         }).catch(done);
     });
