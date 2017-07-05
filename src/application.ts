a2eaefca34dfacd0f20e0ffced6a66fbe86c6f87
Igor Ivanovic <igor.zg1987@gmail.com>
2017-07-05 10:17:01 +0200

    Provide logger as factory

--- src/application.ts
+++ src/application.ts
@@ -1,4 +1,4 @@
-import {Module, Logger, Inject, IAfterConstruct, Router, Methods, LogLevels} from "typeix";
+import {IAfterConstruct, Inject, Logger, LogLevels, Methods, Module, Router} from "typeix";
 import {Assets} from "./components/assets";
 import {CoreController} from "./controllers/core";
 import {HomeController} from "./controllers/home";
@@ -16,90 +16,95 @@ import {DynamicRouteRule} from "./components/dynamic-router";
  * \@Module is used to define application entry point class
  */
 @Module({
-  imports: [AdminModule], // bootstrap in recursive top level order
-  controllers: [HomeController, CoreController], // no order
-  providers: [Logger, Router, Assets, TemplateEngine, InMemoryCache] // in order processed
+    imports: [AdminModule], // bootstrap in recursive top level order
+    controllers: [HomeController, CoreController], // no order
+    providers: [{
+        provide: Logger,
+        useFactory: () => {
+            let logger: Logger = new Logger();
+            logger.enable();
+            logger.printToConsole();
+            logger.setDebugLevel(LogLevels.BENCHMARK);
+            return logger;
+        },
+        deps: []
+    }, Router, Assets, TemplateEngine, InMemoryCache] // in order processed
 })
 export class Application implements IAfterConstruct {
 
-  /**
-   * @param {Assets} assetLoader
-   * @description
-   * Custom asset loader service
-   */
-  @Inject(Assets)
-  assetLoader: Assets;
+    /**
+     * @param {Assets} assetLoader
+     * @description
+     * Custom asset loader service
+     */
+    @Inject(Assets)
+    assetLoader: Assets;
 
-  /**
-   * @param {Logger} logger
-   * @description
-   * Logger service
-   */
-  @Inject(Logger)
-  logger: Logger;
+    /**
+     * @param {Logger} logger
+     * @description
+     * Logger service
+     */
+    @Inject(Logger)
+    logger: Logger;
 
-  /**
-   * @param {Router} router
-   * @description
-   * Router service
-   */
-  @Inject(Router)
-  router: Router;
+    /**
+     * @param {Router} router
+     * @description
+     * Router service
+     */
+    @Inject(Router)
+    router: Router;
 
-  /**
-   * @function
-   * @name Application#afterConstruct
-   *
-   * @description
-   * After construct use injected values to define some behavior at entry point
-   * Defining main route, all routes are processed
-   */
-  afterConstruct() {
+    /**
+     * @function
+     * @name Application#afterConstruct
+     *
+     * @description
+     * After construct use injected values to define some behavior at entry point
+     * Defining main route, all routes are processed
+     */
+    afterConstruct() {
 
-    this.logger.enable();
-    this.logger.printToConsole();
-    this.logger.setDebugLevel(LogLevels.BENCHMARK);
-    this.logger.info("Application.arg", this.assetLoader);
+        this.router.addRules([
+            {
+                methods: [Methods.GET],
+                route: "core/favicon",
+                url: "/favicon.ico"
+            },
+            {
+                methods: [Methods.GET],
+                route: "core/assets",
+                url: "/assets/<file:(.*)>"
+            },
+            {
+                methods: [Methods.GET],
+                route: "home/id",
+                url: "/<id:(\\d+)>/<name:(\\w+)>"
+            },
+            {
+                methods: [Methods.GET],
+                route: "home/id",
+                url: "/<id:(\\d+)>"
+            },
+            {
+                methods: [Methods.GET],
+                route: "home/index",
+                url: "/"
+            },
+            {
+                methods: [Methods.GET],
+                route: "home/redirect",
+                url: "/redirect-to-home"
+            },
+            {
+                methods: [Methods.GET],
+                route: "core/error",
+                url: "/throw-error"
+            }
+        ]);
 
-    this.router.addRules([
-      {
-        methods: [Methods.GET],
-        route: "core/favicon",
-        url: "/favicon.ico"
-      },
-      {
-        methods: [Methods.GET],
-        route: "core/assets",
-        url: "/assets/<file:(.*)>"
-      },
-      {
-        methods: [Methods.GET],
-        route: "home/id",
-        url: "/<id:(\\d+)>/<name:(\\w+)>"
-      },
-      {
-        methods: [Methods.GET],
-        route: "home/id",
-        url: "/<id:(\\d+)>"
-      },
-      {
-        methods: [Methods.GET],
-        route: "home/index",
-        url: "/"
-      },
-      {
-        methods: [Methods.GET],
-        route: "home/redirect",
-        url: "/redirect-to-home"
-      },
-      {
-        methods: [Methods.GET],
-        route: "core/error",
-        url: "/throw-error"
-      }
-    ]);
-
-    this.router.addRule(DynamicRouteRule);
-    this.router.setError("core/error");
-  }
+        this.router.addRule(DynamicRouteRule);
+        this.router.setError("core/error");
+    }
 }
